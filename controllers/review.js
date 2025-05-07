5f3fc24a19c6ec790278dcf977f7ca1fd7834c64
Aadesh Phadake <140011316+Aadesh-Phadake@users.noreply.github.com>
2025-05-07 12:06:46 +0530

    Revert "added payment page"

--- controllers/review.js
+++ controllers/review.js
@@ -1,7 +1,5 @@
 const Listing = require('../models/listing');
 const Review = require('../models/review');
-const expressError = require('../utils/expressError');
-const { reviewSchema } = require('../schema');
 
 module.exports.postReview = async (req, res) => {
     let listing = await Listing.findById(req.params.id);
@@ -20,39 +18,4 @@ module.exports.deleteReview = async (req, res) => {
     await Review.findByIdAndDelete(reviewId);
     req.flash('error', 'Review deleted successfully!');
     res.redirect(`/listings/${id}`);
-};
-
-module.exports.createReview = async (req, res) => {
-    const listing = await Listing.findById(req.params.id)
-        .populate('bookings');
-    
-    if (!listing) {
-        req.flash('error', 'Listing not found');
-        return res.redirect('/listings');
-    }
-
-    // Check if user has booked and stayed at the property
-    const hasBooked = listing.bookings.some(booking => 
-        booking.user.equals(req.user._id) && 
-        new Date(booking.checkOut) < new Date()
-    );
-
-    if (!hasBooked) {
-        req.flash('error', 'You can only review properties you have stayed at');
-        return res.redirect(`/listings/${listing._id}`);
-    }
-
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new expressError(400, msg);
-    }
-
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    listing.reviews.push(review);
-    await review.save();
-    await listing.save();
-    req.flash('success', 'Created new review!');
-    res.redirect(`/listings/${listing._id}`);
 };
